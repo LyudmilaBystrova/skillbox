c3bf8c60375d3e7f5cec36b49b42c8d18dddc8a1
LyudmilaBystrova <ludmilabystrova@gmail.com>
2022-08-02 11:39:28 +0200

    nothing change

---
+++ js training/training cycles.js
@@ -0,0 +1,4 @@
+let a = "4326";
+let b = "297515";
+console.log((a + b).split("").reverse().join("") + "Иванов");
+
---
+++ script.js
@@ -0,0 +1,4 @@
+let a = "This is a beautiful";
+let b = "day";
+let c = a + b;
+console.log(c.indexOf(b));
